6f96842d4603603dc12c7cc74cbff2dde59925ac
Ali Muhammad Asad <93670153+AliMuhammadAsad@users.noreply.github.com>
2023-07-26 23:37:34 +0500

    Automation Script

--- script.py
+++ script.py
@@ -15,3 +15,9 @@ cont = re.sub(r'\[([^\]]+)\]\(([^)]+)\)', update_link, cont)
 
 with open(out, 'w') as f:
     f.write(cont)
+
+# Previously the .md file had links in the standard markdown format of [name](url).
+# I had updated all links in this format, but on creating the site, the links would not open in a new tab
+# and markdown doesn't have support for such things, but HTML does.
+# But it was tedious to change each and every link with <a></a> tags, hence this script automates that process for all the links.
+# Now each link opens in a new tab on the website (not in the markdown though since markdown doesn't support it)
